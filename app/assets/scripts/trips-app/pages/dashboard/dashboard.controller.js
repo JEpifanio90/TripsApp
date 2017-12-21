186bdf90780f62edc7820156f0d5d70b30ec4c7d
Jose <jose.epifanio90@gmail.com>
2017-12-20 20:45:54 -0600

    Done with dashboard TODO: refactor and stuff

--- app/assets/scripts/trips-app/pages/dashboard/dashboard.controller.js
+++ app/assets/scripts/trips-app/pages/dashboard/dashboard.controller.js
@@ -8,10 +8,18 @@
         var dashboardScope = this;
         dashboardScope.trips = [];
         dashboardScope.currentTrip = {};
+        dashboardScope.center = [40.74, -74.18];
         getTrips();
+
+        dashboardScope.findInMap = function(trip) {
+            dashboardScope.center = [trip.lat, trip.lng];
+        };
 
         NgMap.getMap('tripMap').then(function(map) {
             dashboardScope.mapInstance = map;
+            dashboardScope.trips.forEach( trip => {
+                dashboardScope.mapInstance.hideInfoWindow(trip.infoWindowId, trip.markerId);
+            });
         });
 
         function getTrips() {
